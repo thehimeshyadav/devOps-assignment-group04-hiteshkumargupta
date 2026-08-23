0fd709c80f77bb5656e5ec4fe0f1ed09a70ecbbd
Himesh Yadav <himesh ydv@gmail.com>
2026-08-23 21:55:40 +0530

    Setup movie recommendation frontend

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,28 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import { Toaster } from 'react-hot-toast';
-import App from './App';
-import { AuthProvider } from './context/AuthContext';
-import './index.css';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        {/* Global toast notifications */}
-        <Toaster
-          position="top-right"
-          toastOptions={{
-            duration: 3000,
-            style: {
-              fontSize: '14px',
-              fontFamily: 'Inter, sans-serif',
-            },
-          }}
-        />
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
-  </React.StrictMode>
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
